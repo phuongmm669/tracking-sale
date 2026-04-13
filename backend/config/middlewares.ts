132c54d18467d15987a2d576c4c2a644a537bba0
Le Hoang Minh Phuong <phuong.le@hellohealthgroup.com>
2026-04-13 17:42:06 +0700

    config

--- backend/config/middlewares.ts
+++ backend/config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000", "https://tracking-sale.vercel.app"], // Thêm link Vercel của bạn vào đây
+    },
+  },
 ];
--- backend/import-customers.js
+++ backend/import-customers.js
@@ -1,10 +1,14 @@
 // import-customers.js
+require('dotenv').config({ path: '.env' });
+
 const axios = require('axios');
 const fs = require('fs');
 const { parse } = require('csv-parse');
 
-const STRAPI_API = 'http://127.0.0.1:1337/api/customers';
-const STRAPI_TOKEN = '2a2bc170f8897f2d7cd8fa660af1b4b258f911a49209ecdbe1e91ef3e6ff4e8d9367c8fc875d691a8bfecbbbcfa2bc4d3750a1ff36beeecf4edea6ba8967097c8f03edb09ca723209c592ee2adce41eb2a7acd5eaa16d34d9c27b9aa82a20fac759e0487a0ba67bcec5060c9824507d593d988c3a7542b96a1d6cf98a286f745'; // Tạo trong Settings -> API Tokens
+const STRAPI_BASE_URL =
+  process.env.STRAPI_URL || `http://127.0.0.1:${process.env.PORT || 1337}`;
+const STRAPI_API = `${STRAPI_BASE_URL}/api/customers`;
+const STRAPI_TOKEN = process.env.CUSTOMER_TOKEN;
 
 async function importData() {
   const parser = fs.createReadStream('./data.csv').pipe(parse({ columns: true }));
